198788ebe8ce464f6dcf6fea1e85a7c2c9293658
gshaver <geneshaver@gmail.com>
2023-02-19 16:14:16 -0600

    Update forms.js

--- screens/forms.js
+++ screens/forms.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { View, TextInput, TouchableOpacity, Text } from 'react-native';
 import { Formik } from 'formik';
+import * as Yup from 'yup'; // import Yup for validation
 import { globalStyles } from '../styles/global';
+
+const validationSchema = Yup.object().shape({
+    name: Yup.string()
+        .min(2, 'Too short!')
+        .required('Name is required'),
+    phoneNumber: Yup.string()
+        .min(10, 'Too short!')
+        .max(10, 'Too long!')
+        .required('Phone number is required'),
+});
 
 const SignupForm = () => {
     return (
         <Formik
             initialValues={{ name: '', phoneNumber: '' }}
+            validationSchema={validationSchema} // add validation schema
             onSubmit={(values) => console.log(values)}
         >
-            {({ handleChange, handleBlur, handleSubmit, values }) => (
+            {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
                 <View>
                     <TextInput
                         style={globalStyles.input}
@@ -18,6 +30,9 @@ const SignupForm = () => {
                         onBlur={handleBlur('name')}
                         value={values.name}
                     />
+                    {touched.name && errors.name && (
+                        <Text style={globalStyles.errorText}>{errors.name}</Text>
+                    )}
                     <TextInput
                         style={globalStyles.input}
                         placeholder="phoneNumber"
@@ -26,6 +41,9 @@ const SignupForm = () => {
                         value={values.phoneNumber}
                         keyboardType="numeric"
                     />
+                    {touched.phoneNumber && errors.phoneNumber && (
+                        <Text style={globalStyles.errorText}>{errors.phoneNumber}</Text>
+                    )}
                     <TouchableOpacity
                         style={[globalStyles.TouchableOpacitybutton, { flexDirection: 'row' }]}
                         onPress={handleSubmit}
